f0c90afe49f57d798448cc365878751a23b01eb2
crullian <crullian@gmail.com>
2018-02-11 19:23:30 -0500

    Handle case for refresh action in reducer.

--- src/state/reducer.js
+++ src/state/reducer.js
@@ -3,6 +3,8 @@ import { combineReducers } from 'redux'
 function songs (state = [], action) {
   switch (action.type) {
     case 'GET_DOORSTEPS_SONGS_SUCCESS':
+    case 'REFRESH_DOORSTEPS_SONGS_SUCCESS':
+      console.log('%c  ACTION ->', 'color:chartreuse', action);
       return action.songs
 
     default:
