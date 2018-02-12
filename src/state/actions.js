f7b5baa6ba2e918aa403260c3470f8f9d544d750
crullian <crullian@gmail.com>
2018-02-11 19:20:13 -0500

    Create refresh action on client.

--- src/state/actions.js
+++ src/state/actions.js
@@ -1,9 +1,20 @@
-export function getDoorstepsSongs() {
+export const getDoorstepsSongs = () => {
   return async dispatch => {
-    const { songs } = await fetch('http://localhost:4000/songs').then(res => res.json())
+    const songs = await fetch('http://localhost:4000/songs').then(res => res.json())
 
     return dispatch({
       type: 'GET_DOORSTEPS_SONGS_SUCCESS',
+      songs,
+    })
+  }
+}
+
+export const refreshSongs = () => {
+  return async dispatch => {
+    const songs = await fetch('http://localhost:4000/refreshSongs').then(res => res.json())
+
+    return dispatch({
+      type: 'REFRESH_DOORSTEPS_SONGS_SUCCESS',
       songs,
     })
   }
